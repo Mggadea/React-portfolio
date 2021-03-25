7ce9c54f79657af6473ff08430b36fa0631aafc1
matias <matias@enve.io>
2021-03-25 18:37:41 -0300

    Center Title

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -9,16 +9,16 @@ const About = () => {
       <div className="about">
         <h1 className="about-title">About me.</h1>
         <p className="about-text">
-         I’m a seasoned fullstack developer based in Buenos Aires AR, with great experience in Front-end
-          with React and the DevOps field. I also have deep knowledge
-          implementing complex architectures in cloud environments. I've helped
-          several organizations to introduce cultural changes, best practices,
-          and new technologies to improve collaboration between business,
-          development, and operations and create an agile application lifecycle.{" "}
+          I’m a seasoned fullstack developer based in Buenos Aires AR, with
+          great experience in Front-end with React and the DevOps field. I also
+          have deep knowledge implementing complex architectures in cloud
+          environments. I've helped several organizations to introduce cultural
+          changes, best practices, and new technologies to improve collaboration
+          between business, development, and operations and create an agile
+          application lifecycle.{" "}
         </p>
-        <h1 className="about-title">Skills.</h1>
+      
       </div>
-     
     </Container>
   );
 };
