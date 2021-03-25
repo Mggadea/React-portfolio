1eed15d9cf062edc3d9433b4bd4c643cd6283c6a
matias <matias@enve.io>
2021-03-25 15:00:07 -0300

    Progress in My work section

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,24 +1,79 @@
 import React from "react";
 import Container from "@material-ui/core/Container";
+import Button from "@material-ui/core/Button";
 import "./Projects.css";
 import Grid from "@material-ui/core/Grid";
-import project1 from "../../img/project-1.PNG";
+import project2 from "../../img/project-2.PNG";
 
 const Projects = () => {
   return (
     <Container maxWidth="lg">
       <div className="projects">
-        <h1 className="projectstitle"> My work.</h1>
+        <h1 className="projects__title"> My work.</h1>
         <Grid container spacing={3}>
-          <Grid item xs={12} sm={6}></Grid>
           <Grid item xs={12} sm={6}>
-            <p>
+            <img src={project2} className="projects-img" alt=""/>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <p className="projects__description">
               Facebook es una red social que fue creada para poder mantener en
               contacto a personas, y que éstos pudieran compartir información,
               noticias y contenidos audiovisuales con sus propios amigos y
               familiares. Es uno de los canales digital más conocidos por todos
               los usuarios que navegan hoy en día por Internet
             </p>
+            <Button variant="contained" className="projects__button">Source Code</Button>
+            <Button variant="contained" color="primary">Live Url</Button>
+          </Grid>
+        </Grid>
+        <Grid container spacing={3}>
+          
+          <Grid item xs={12} sm={6}>
+            <p className="projects__description">
+              Facebook es una red social que fue creada para poder mantener en
+              contacto a personas, y que éstos pudieran compartir información,
+              noticias y contenidos audiovisuales con sus propios amigos y
+              familiares. Es uno de los canales digital más conocidos por todos
+              los usuarios que navegan hoy en día por Internet
+            </p>
+            <Button variant="contained" className="projects__button">Source Code</Button>
+            <Button variant="contained" color="primary">Live Url</Button>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <img src={project2} className="projects-img" alt=""/>
+          </Grid>
+        </Grid>
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={6}>
+            <img src={project2} className="projects-img" alt=""/>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <p className="projects__description">
+              Facebook es una red social que fue creada para poder mantener en
+              contacto a personas, y que éstos pudieran compartir información,
+              noticias y contenidos audiovisuales con sus propios amigos y
+              familiares. Es uno de los canales digital más conocidos por todos
+              los usuarios que navegan hoy en día por Internet
+            </p>
+            <Button variant="contained" className="projects__button">Source Code</Button>
+            <Button variant="contained" color="primary">Live Url</Button>
+          </Grid>
+        </Grid>
+        <Grid container spacing={3}>
+          
+          <Grid item xs={12} sm={6}>
+            <p className="projects__description">
+              Facebook es una red social que fue creada para poder mantener en
+              contacto a personas, y que éstos pudieran compartir información,
+              noticias y contenidos audiovisuales con sus propios amigos y
+              familiares. Es uno de los canales digital más conocidos por todos
+              los usuarios que navegan hoy en día por Internet
+            </p>
+            <Button variant="contained" className="projects__button">Source Code</Button>
+            <Button variant="contained" color="primary">Live Url</Button>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <img src={project2} className="projects-img" alt=""/>
           </Grid>
         </Grid>
       </div>
